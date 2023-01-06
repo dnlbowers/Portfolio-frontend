dd249327242ef3bd7b53c2b8b3c224f013543fd0
dnlbowers <dbowers@greentube.com>
2023-01-06 11:44:41 +0100

    swap out ngx bootstrap for ngbootsrap again due to compatability issues with Angular 15

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -15,9 +15,8 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    CollapseModule.forRoot(),
   ],
   
   providers: [],
