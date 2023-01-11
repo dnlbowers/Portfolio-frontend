d9b0fcd701dc28df80fdc8792bb5e8d062d172b7
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-01-12 00:15:33 +0100

    finish draft stytle of carousel

--- client/src/app/project-carousel/project-carousel.component.ts
+++ client/src/app/project-carousel/project-carousel.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, ViewChild } from '@angular/core';
-import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarousel, NgbCarouselConfig, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import { Project } from '../_models/project';
 
@@ -9,26 +9,33 @@ import { Project } from '../_models/project';
   styleUrls: ['./project-carousel.component.css'],
 })
 export class ProjectCarouselComponent {
-  @Input() projects: Observable<Project[]> | undefined;
+  	@Input() projects: Observable<Project[]> | undefined;
   
 
-  paused = false;
+  	paused = false;
 	unpauseOnArrow = true;
 	pauseOnIndicator = false;
 	pauseOnHover = true;
 	pauseOnFocus = false;
+	showNavigationIndicators = false;
 
-  @ViewChild('carousel', { static: true })
-  carousel!: NgbCarousel;
-  
-  togglePaused() {
-		if (this.paused) {
-			this.carousel.cycle();
-		} else {
-			this.carousel.pause();
-		}
-		this.paused = !this.paused;
+	@ViewChild('carousel', { static: true })
+	carousel!: NgbCarousel;
+
+	constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.showNavigationArrows = true;
+		config.showNavigationIndicators = false;
 	}
+	
+	togglePaused() {
+			if (this.paused) {
+				this.carousel.cycle();
+			} else {
+				this.carousel.pause();
+			}
+			this.paused = !this.paused;
+		}
 
 	onSlide(slideEvent: NgbSlideEvent) {
 		if (
