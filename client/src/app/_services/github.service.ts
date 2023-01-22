91effbb610b7aeb226637808d830366fe0447682
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-01-22 23:57:32 +0100

    create github profile model, wrtie the service to call api and logic to use service in the component

--- client/src/app/_services/github.service.ts
+++ client/src/app/_services/github.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of, tap } from 'rxjs';
+import { GithubProfile } from '../_models/github-profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  baseUrl: string = "https://api.github.com/users/dnlbowers";
+  profile: GithubProfile | undefined
+
+  constructor(private http: HttpClient) { }
+
+  getProfile() {
+    if(this.profile) return of(this.profile);
+    return this.http.get<GithubProfile>(this.baseUrl)
+        .pipe(
+            tap(profile => this.profile = profile)
+        );
+    }
 }
--- client/src/app/github-repos/github-repos.component.ts
+++ client/src/app/github-repos/github-repos.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GithubProfile } from '../_models/github-profile';
+import { GithubService } from '../_services/github.service';
 
 @Component({
   selector: 'app-github-repos',
   templateUrl: './github-repos.component.html',
   styleUrls: ['./github-repos.component.css']
 })
-export class GithubReposComponent {
-
+export class GithubReposComponent implements OnInit {
+  
   @Input() headerColor!: string;
+
+  profile: GithubProfile | undefined = undefined;
+
+  constructor(private githubService: GithubService) { }
+
+  ngOnInit(): void {
+    this.githubService.getProfile().subscribe(data => {
+      this.profile = data;
+    });
+  };
 }
+
+
+
