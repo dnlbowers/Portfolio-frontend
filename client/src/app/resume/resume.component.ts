85d1ae500d0853549406adf9a6d1ce31f74d79f1
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-01-25 00:07:57 +0100

    Fix styling on screensize change for resume, tweak styling across all CV to be more uniform

--- client/src/app/resume/resume.component.ts
+++ client/src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faGears, faTv } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -6,11 +6,30 @@ import { faGears, faTv } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit {
   
   faTv = faTv
   faGears = faGears
 
   @Input() headerColor!: string;
+  workColors: string | undefined;
+  skillsColors: string | undefined;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.styleUpdate();
+    window.onresize = () => this.styleUpdate();
+  }
+
+  styleUpdate() {
+    if (window.innerWidth < 992) {
+      this.workColors = "bg-light text-black border border-dark border-5 border-top-0";
+      this.skillsColors = "bg-dark text-light ";
+    } else {
+      this.workColors = "bg-black text-light";
+      this.skillsColors = "bg-light text-black border border-dark border-5 border-top-0"
+    }
+  }
 
 }
