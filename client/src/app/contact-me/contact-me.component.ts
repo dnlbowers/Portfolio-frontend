df6abc94e8942f72858fc8f02562cc995cb87224
dnlbowers <dbowers@greentube.com>
2023-02-03 19:07:23 +0100

    add form validation

--- client/src/app/contact-me/contact-me.component.ts
+++ client/src/app/contact-me/contact-me.component.ts
@@ -12,13 +12,40 @@ export class ContactMeComponent {
     reasons = ['Hire me', 'Question', 'Comment/Feedback', 'Other'];
 
     submitted = false;
+    confirmed = false;
+    sentToApi = false;
+    processing = false;
 
     model = new ContactForm('', '', this.reasons[0], '');
 
     onSubmit(){
-       this.submitted = true;
-       console.log(this.model);
-       console.log(this.submitted)
+      this.submitted = true;
+      this.processing = true;
+      console.log(this.model);
+      console.log(this.submitted)
+    }
+
+    editSubmission() {
+      this.processing = false;
+      this.submitted = false;
+    }
+
+    sendMessage() {
+      this.confirmed = true;
+      this.submitted = false;
+      //logic to send to the api here
+
+      // then only if api call successful these actions should be taken 
+      this.sentToApi = true;
+      this.model = new ContactForm('', '', this.reasons[0], '');
+    }
+
+    sendNewMessage() {
+      this.processing = false;
+      
+      this.confirmed = false;
+      this.sentToApi = false;
+      
     }
 
 }
