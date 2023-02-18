9c1298066b96bd5ad7686b665a76faa5f52f4d93
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-19 00:59:14 +0100

    amend api urls

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    contactsApiUrl: 'http://localhost:8000/api/contacts/contact-requests/',
+    contactsApiUrl: 'http://localhost:8000/api/contact/contact-requests/',
     projectsApiUrl: 'http://localhost:8000/api/portfolio/',
     gitHunApiUrl: 'https://api.github.com/users/dnlbowers',
     cloudinaryImageUrl: 'https://res.cloudinary.com/df4m8hva5/',
