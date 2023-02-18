196b4fcd5e4657e59e7d99b6b0fe271099f47ea1
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-19 00:38:04 +0100

    change server port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/your-app-name/index.html'));
 });
 
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '5000';
 app.set('port', port);
 
 const server = http.createServer(app);
