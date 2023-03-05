be2f0cafca59e277ad4646149a897a7bba454db5
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-03-05 01:01:48 +0100

    animate navbar on mobile screens

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,9 +1,27 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { faCode, faLaptopCode, faBars } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'app-nav',
+  animations: [
+    trigger('mobileMenuAnimation', [
+      state('open', style({
+        // open nav menu fully
+        transform: 'translateX(0)',
+        height: '*',
+      })),
+      state('closed', style({
+        // close nav menu fully
+        height: '0',
+        overflow: 'hidden',
+        transform: 'translateX(-100%)'
+      })),
+      transition('open => closed', animate('0.3s ease-in')),
+      transition('closed => open', animate('0.5s ease-out'))
+    ])
+  ],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
@@ -30,6 +48,10 @@ export class NavComponent implements OnInit {
   }
   activeParentTrue() {
     this.makeParentActive = true;
+  }
+
+  toggle() {
+    this.isCollapsed = !this.isCollapsed;
   }
   
 }
